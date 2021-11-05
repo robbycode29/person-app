fd88e41394e4aeef3666f8ec50d7f565f84d55c8
tudorPopescu <tudorp2@gmail.com>
2021-11-05 11:46:48 +0200

    initial

---
+++ server/controllers/informationCtrl.js
@@ -0,0 +1,23 @@
+module.exports = db => {
+  return {
+    create: (req, res) => {
+
+    },
+
+    update: (req, res) => {
+
+    },
+
+    findAll: (req, res) => {
+
+    },
+
+    find: (req, res) => {
+
+    },
+
+    destroy: (req, res) => {
+
+    }
+  };
+};
