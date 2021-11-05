895a385bcd5870d76f374d008adc2d7b86032b59
tudorPopescu <tudorp2@gmail.com>
2021-11-05 14:26:16 +0200

    config

---
+++ client/src/app/components/information/information-modal/information-modal.component.ts
@@ -0,0 +1,51 @@
+import axios from 'axios';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { REPLACE_DIACRITICS } from 'src/app/utils/utils-input';
+import { toastr } from '../../toastr/toastr.component';
+
+@Component({
+  selector: 'app-information-modal',
+  templateUrl: './information-modal.component.html'
+})
+export class InformationModalComponent implements OnInit {
+  @Input() id_information: number | undefined;
+
+  modal = {} as any;
+
+  constructor(private _spinner: NgxSpinnerService, public activeModal: NgbActiveModal) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  save(): void {
+    this._spinner.show();
+    toastr.success('ASD')
+
+    if (!this.id_information) {
+      axios.post('/api/information', this.modal).then(() => {
+        this._spinner.hide();
+        toastr.success('Informația a fost salvată cu succes!');
+      }).catch(() => toastr.error('Eroare la salvarea informației!'));
+    } else {
+      axios.put('/api/information', this.modal).then(() => {
+        this._spinner.hide();
+        toastr.success('Informația a fost modificată cu succes!');
+      }).catch(() => toastr.error('Eroare la modificarea informației!'));
+    }
+  }
+
+  selectSearch(term: string, item: any): boolean {
+    const isWordThere = [] as any;
+    const splitTerm = term.split(' ').filter(t => t);
+
+    item = REPLACE_DIACRITICS(item.name);
+
+    splitTerm.forEach(term => isWordThere.push(item.indexOf(REPLACE_DIACRITICS(term)) !== -1));
+    const all_words = (this_word: any) => this_word;
+
+    return isWordThere.every(all_words);
+  }
+}
